072b29a6ebcc1ed6b285ce61602784620133fc52
ALEXX4891 <alexx4891@mail.ru>
2025-05-15 20:20:38 +0500

    Вернет роуты обратно

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\V1\NotificationController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -11,8 +10,6 @@ Route::get('/', function () {
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::post('/notification', [NotificationController::class, 'index'])->middleware('checkIp');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
